a5956269bc479e76f35e755b473d4374c70e75ec
snerks <martin.github@outlook.com>
2014-04-30 17:35:26 +0100

    Added Sample1
    Added Sample1.html

---
+++ scripts/app/sampleClass.ts
@@ -0,0 +1,15 @@
+﻿class SampleClass {
+    numberValue: number;
+
+    constructor(numberValue: number) {
+        this.numberValue = numberValue;
+    }
+
+    setNumber(numberValue: number) {
+        this.numberValue = numberValue;
+    }
+
+    getSquare() {
+        return this.numberValue * this.numberValue;
+    }
+} 
---
+++ scripts/test/unit/personCtrlSpec.js
@@ -0,0 +1,48 @@
+﻿/// <reference path="../../app/app1.ts" />
+/// <reference path="../../app/personCtrl.ts" />
+/// <reference path="../../typings/jasmine/jasmine.d.ts" />
+describe('personCtrl', function () {
+    it('should have correct static controllerId', function () {
+        // Arrange
+        // Act
+        var result = personCtrl.staticControllerId;
+
+        // Assert
+        expect(result).toBe("personCtrl");
+    });
+
+    it('should create default model with correct instance controllerId', function () {
+        // Arrange
+        var scope = {}, sut = new personCtrl(scope);
+
+        // Act
+        var result = sut.controllerId;
+
+        // Assert
+        expect(result).toBe("personCtrl");
+    });
+
+    it('should have correct greeting', function () {
+        // Arrange
+        var scope = {}, sut = new personCtrl(scope);
+
+        // Act
+        var result = scope.greeting;
+
+        // Assert
+        expect(result).toBe("Good day.");
+    });
+
+    it('changeGreeting should set correct greeting', function () {
+        // Arrange
+        var scope = {}, sut = new personCtrl(scope);
+
+        // Act
+        sut.changeGreeting();
+        var result = scope.greeting;
+
+        // Assert
+        expect(result).toBe("Bye");
+    });
+});
+//# sourceMappingURL=personCtrlSpec.js.map
